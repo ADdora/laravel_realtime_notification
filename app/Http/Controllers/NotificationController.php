5e0c450e86ac49de29c021a9e9218b7f7728b5e2
Mahmoud <prog.mahmoud96@hotmail.com>
2018-11-13 20:54:20 +0200

    NotificationController Send

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\DatabaseNotification;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
     public function send()
     {
+        // broadcast the event
 
+        $data = [
+            'name' => 'test',
+        ];
+        event(new DatabaseNotification($data));
     }
 }
