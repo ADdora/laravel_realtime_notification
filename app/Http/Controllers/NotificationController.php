d5f9e246a0bcfe7cbd361696f660881a792e109c
Mahmoud <prog.mahmoud96@hotmail.com>
2018-11-18 14:39:32 +0200

    Refactoring NotificationController

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,18 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Events\DatabaseNotification;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
     public function send()
     {
-        // broadcast the event
-
         $data = [
-            'user_id' => Auth::id(),
-            'title' => 'test',
+            'user_id'    => Auth::id(),
+            'title'      => 'test',
             'created_at' => Carbon::now()->toDateTimeString(),
         ];
         event(new DatabaseNotification($data));
